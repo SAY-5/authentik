a846474c15deeb4d59a1c0d8eeff3287853967b8
Dominic R <dominic@sdko.org>
2026-03-14 00:12:55 -0400

    core, web: More work

--- authentik/tenants/api/settings.py
+++ authentik/tenants/api/settings.py
@@ -73,7 +73,6 @@ class SettingsSerializer(ModelSerializer):
             "pagination_default_page_size",
             "pagination_max_page_size",
             "security_email",
-            "account_lockdown_enabled",
             "flags",
         ]
 
