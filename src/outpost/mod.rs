915bf6942eda4b067fecd53651c17b9587578bb6
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-04-10 17:16:32 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- src/outpost/mod.rs
+++ src/outpost/mod.rs
@@ -1,37 +1,43 @@
 use ak_client::apis::root_api::root_config_retrieve;
 use ak_client::apis::{configuration::Configuration, outposts_api::outposts_instances_list};
 use ak_client::models::Config;
+use ak_client::models::Outpost as OutpostModel;
 use ak_common::{Tasks, config};
 use eyre::{Result, eyre};
+use std::sync::Arc;
 use std::time::Duration;
 use tokio_retry2::strategy::FixedInterval;
 use tokio_retry2::{Retry, RetryError};
-use tracing::{debug, error, warn};
+use tracing::{debug, error};
 
 #[cfg(feature = "proxy")]
 pub(crate) mod proxy;
 
-pub(crate) trait Outpost {
+pub(crate) trait Outpost: Sized {
     type Cli;
+
+    async fn new(controller: Arc<OutpostController>) -> Result<Self>;
+
+    async fn refresh(&self) -> Result<()>;
 }
 
 #[derive(Debug)]
-struct OutpostController {
+pub(crate) struct OutpostController {
     api_config: Configuration,
     root_config: Config,
-}
-
-fn retry_notify_new(
-    err: &ak_client::apis::Error<ak_client::apis::outposts_api::OutpostsInstancesListError>,
-    _duration: Duration,
-) {
-    warn!(
-        ?err,
-        "Failed to fetch outpost configuration, retrying in 3 seconds"
-    );
+    outpost: OutpostModel,
+    ak_host: String,
 }
 
 impl OutpostController {
+    pub(crate) fn is_embedded(&self) -> bool {
+        self.outpost
+            .managed
+            .as_ref()
+            .and_then(|m| m.as_deref())
+            .is_some_and(|m| m == "goauthentik.io/outposts/embedded")
+    }
+
     async fn new(ak_host: String, ak_token: String) -> Result<Self> {
         let api_config = Configuration {
             base_path: format!("{ak_host}/api/v3"),
@@ -72,7 +78,7 @@ impl OutpostController {
             Retry::spawn_notify(retry_strategy, retrieve_outposts, retry_notify).await?
         };
 
-        let Some(outpost) = outposts.results.first() else {
+        let Some(outpost) = outposts.results.into_iter().next() else {
             return Err(eyre!(
                 "No outposts found with given token, ensure the given token corresponds to an authentik Outpost"
             ));
@@ -88,6 +94,8 @@ impl OutpostController {
         Ok(Self {
             api_config,
             root_config,
+            outpost,
+            ak_host,
         })
     }
 }
@@ -102,7 +110,9 @@ pub(crate) async fn run<O: Outpost>(_cli: O::Cli, _tasks: &mut Tasks) -> Result<
         .clone()
         .ok_or_else(|| eyre!("environment variable `AUTHENTIK_TOKEN` not set"))?;
 
-    let controller = OutpostController::new(ak_host, ak_token).await?;
-    dbg!(controller);
+    let controller = Arc::new(OutpostController::new(ak_host, ak_token).await?);
+    let outpost = Arc::new(O::new(Arc::clone(&controller)).await?);
+    outpost.refresh().await?;
+
     Ok(())
 }
--- src/outpost/proxy/mod.rs
+++ src/outpost/proxy/mod.rs
@@ -1,6 +1,12 @@
+use ak_client::apis::outposts_api::outposts_proxy_list;
+use eyre::Result;
+use std::sync::Arc;
+
 use argh::FromArgs;
 
 use crate::outpost::Outpost;
+use crate::outpost::OutpostController;
+use tracing::warn;
 
 #[derive(Debug, Default, FromArgs, PartialEq, Eq)]
 /// Run the authentik proxy outpost.
@@ -11,8 +17,28 @@ use crate::outpost::Outpost;
 )]
 pub(crate) struct Cli {}
 
-pub(crate) struct ProxyOutpost;
+pub(crate) struct ProxyOutpost {
+    controller: Arc<OutpostController>,
+}
 
 impl Outpost for ProxyOutpost {
     type Cli = Cli;
+
+    async fn new(controller: Arc<OutpostController>) -> Result<Self> {
+        Ok(Self { controller })
+    }
+
+    async fn refresh(&self) -> Result<()> {
+        let providers =
+            outposts_proxy_list(&self.controller.api_config, None, None, None, None, None)
+                .await?
+                .results;
+        if providers.is_empty() {
+            warn!(
+                "No providers assigned to this outpost, check outpost configuration in authentik"
+            );
+        }
+
+        Ok(())
+    }
 }
